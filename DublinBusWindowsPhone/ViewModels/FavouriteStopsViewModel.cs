3dfd455f50c958747b9df1b2b8adcbf84c540157
ArturPhilibin <dasilva.artur@gmail.com>
2013-02-04 20:35:22 +0000

    Work I forgot to commit, can't remember what changed

---
+++ DublinBusWindowsPhone/ViewModels/FavouriteStopsViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using DublinBusWindowsPhone.Helpers;
+using System.IO.IsolatedStorage;
+using System.Collections.ObjectModel;
+
+namespace DublinBusWindowsPhone.ViewModels
+{
+    public class FavouriteStopsViewModel : ViewModelBase
+    {
+        private readonly ObservableCollection<int> favouriteStops;
+
+        public FavouriteStopsViewModel()
+        {
+            this.favouriteStops = new ObservableCollection<int>();
+        }
+
+        private void LoadFavourites()
+        {
+            var storage = IsolatedStorageSettings.ApplicationSettings;
+
+            var stops = storage.Contains("FavouriteStops")
+                ? (int[])storage["FavouriteStops"] 
+                : new int[] { };
+
+            foreach (var stop in stops)
+            {
+                this.FavouriteStops.Add(stop);
+            }
+        }
+
+        public ObservableCollection<int> FavouriteStops 
+        {
+            get
+            {
+                return this.favouriteStops;
+            }
+        }
+    }
+}
---
+++ DublinBusWindowsPhone/Views/BusStop.xaml.cs
@@ -0,0 +1,23 @@
+﻿//-------------------------------------------------------------------------
+// <copyright file="BusStopRealTimeInformationView.xaml.cs">
+//     Copyright (c) Artur Philibin E Silva All rights reserved.
+// </copyright>
+//-------------------------------------------------------------------------
+
+namespace DublinBusWindowsPhone.Views
+{
+    /// <summary>
+    /// Code-behind class for <see cref="BusStopRealTimeInformationView"/>.xaml
+    /// </summary>
+    public partial class BusStopRealTimeInformationView
+    {
+        /// <summary>
+        /// Initializes a new instance of the 
+        /// <see cref="BusStopRealTimeInformationView"/> class
+        /// </summary>
+        public BusStopRealTimeInformationView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
